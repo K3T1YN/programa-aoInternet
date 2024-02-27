132ff7c5661eeb5d6c6586504430376d55badfe7
K3T1YN <99234014+K3T1YN@users.noreply.github.com>
2024-02-27 16:21:02 -0300

    27/02

---
+++ sintaxe.js
@@ -0,0 +1,96 @@
+// Declaração de variáveis
+
+let nome; // (LET) Valor manipilável
+let idade; 
+
+nome = "Ketlyn"; // String
+idade = "19"; // Number
+
+const altura = 1.64; //(CONST) Valor constante
+
+let bol = true /// Boolean
+let lista = ["banana", "maça", "uva"]; // Lista
+let objeto = {nome: "Viviane", idade: 17}; // Objeto
+
+
+// Operadores Aritméticos
+
+let a = 10;
+
+let b = 5;
+
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let resto = a % b;
+
+// Operadores de Atribuição
+let x = 10;
+
+x += 5; // x = x + 5 (15)
+x -= 3; // x = x - 3 (12)
+x *= 2; // x = x * 2 (24)
+x/= 4; // x = x / 4 (06)
+
+
+// Operadores de Comparação Lógica
+
+idade = 20;
+let temCNH = true
+console.log(idade > 18); // idade é maior que 18?
+console.log(idade == 18); // idade é igual a 18?
+console.log(idade !== 18); // idade é diferente a 18?
+
+/* Igualdade solta
+5 == "5" (true)
+
+Igualdade estrita
+5 = "5" (false)
+
+0 == false(true)
+0 === false(false)
+*/
+
+console.log(idade > 18 && temCNH); // idade maior que 18
+console.log(idade > 18 || temCNH); // idade maior que 18
+console.log(!temCNH); // negação de posuir cnh (false)
+
+// Estrutura Condicional
+let hora = 12;
+
+if(hora < 12){
+    console.log("Bom dia");
+} else if (hora >= 12 && hora <= 18) {
+    console.log("Boa tarde");
+} else {
+    console.log("Boa noite");
+}
+
+
+// Estruturas de Repetições
+
+for (lef i=o; i<10; i++){
+    console.log(i);
+}
+
+let contador = 0;
+white (contador < 5) {
+    console.log(contador);
+    contador++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
